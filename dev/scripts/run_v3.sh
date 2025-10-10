e367c3da6aa385ad211cce9f545d94a12c67b6f6
MarcosZyk <1534661820@qq.com>
2025-10-10 02:51:23 +0000

    test tools

---
+++ dev/scripts/run_v3.sh
@@ -0,0 +1,16 @@
+NODE_RANK=$1
+HOST_PORT=$2
+
+export AMX_PARALLEL=1
+
+python3 -m sglang.launch_server \
+    --device cpu \
+    --mem-fraction-static 0.048 \
+    --disable-radix-cache \
+    --disable-overlap-schedule \
+    --model-path /sgl-workspace/deepseek-v3 \
+    --tp 2 \
+    --dist-init-addr 127.0.0.1:20000 \
+    --host 0.0.0.0 --port $HOST_PORT \
+    --nnodes 2 \
+    --node-rank $NODE_RANK
--- dev/test_server.py
+++ dev/test_server.py
@@ -12,5 +12,41 @@ client = OpenAI(
 test_prompt = "Please give some more content based on the following content. San Francisco is a  city that has been around for over 100 years. It was founded in 1849 by the Gold Rush, and it's still growing today. The city is home to many different neighborhoods and districts, each with their own unique character and culture. One of the most popular neighborhoods in San Francisco is Chinatown. This neighborhood is known for its vibrant Chinese-American community, as well as its delicious cuisine and cultural events. If you're visiting San Francisco during your trip, don't miss out on exploring this beloved neighborhood!"
 start_time=time.time()
 completion = client.completions.create(model="deepseek-ai/DeepSeek-V2-Lite-Chat",
-                                      prompt=test_prompt, temperature=0.0, max_tokens=256)
-print(time.time()-start_time, test_prompt, completion.choices[0].text)
+                                      prompt=test_prompt, temperature=0.0, max_tokens=2048)
+# while True:
+print("========Question=======")
+print(test_prompt)
+print("=========Answer========")
+print(time.time()-start_time)
+print(completion.choices[0].text)
+
+response = 0
+dialect = test_prompt
+
+follow_up = [
+    'Please give me more descriptions about its cultural.',
+    'Please give me more descriptions about its style of architecture.',
+    'Please give me more descriptions about the living costs in this city.',
+    'Please give me more descriptions about the famous people from this city.',
+]
+idx = 0
+
+while response < 4 * 1024:
+
+    start_time = time.time()
+    completion = client.completions.create(model="deepseek-ai/DeepSeek-V2-Lite-Chat",
+                                           prompt=dialect, temperature=0.0, max_tokens=8192)
+    print(time.time() - start_time)
+    print(completion.usage.total_tokens)
+    print(completion.choices[0].text)
+
+    response = completion.usage.total_tokens
+    dialect += dialect + '\nHere\'the answer. ' + completion.choices[0].text + '\nAll above are previous questions and answers. ' + follow_up[idx]
+    idx += 1
+
+    if idx == len(follow_up):
+        break
+
+
+
+
