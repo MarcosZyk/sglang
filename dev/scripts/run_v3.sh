e0be968972afa999857491ac88898bd32a8205c7
MarcosZyk <1534661820@qq.com>
2025-10-12 16:32:31 +0800

    fix name

--- dev/scripts/run_v3.sh
+++ dev/scripts/run_v3.sh
@@ -8,7 +8,7 @@ python3 -m sglang.launch_server \
     --mem-fraction-static 0.1 \
     --disable-radix-cache \
     --disable-overlap-schedule \
-    --model-path /sgl-workspace/models/deepseek-v3 \
+    --model-path /sgl-workspace/models/deepseek-r1 \
     --tp 2 \
     --dist-init-addr 127.0.0.1:20000 \
     --host 0.0.0.0 --port $HOST_PORT \
