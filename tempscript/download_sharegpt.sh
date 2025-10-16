0336c639a9b175428d061b7d6dbc59ca00baf430
blossomin <sakura.chdong@gmail.com>
2025-10-16 09:15:13 +0000

    update deployment scripts

---
+++ tempscript/download_sharegpt.sh
@@ -0,0 +1,1 @@
+scp -i id_ed dchen@143.89.76.94:/home/dchen/projects/ShareGPT_V3_unfiltered_cleaned_split.json .
--- tempscript/launch_sgl.sh
+++ tempscript/launch_sgl.sh
@@ -1,6 +1,15 @@
-export SGLANG_USE_CPU_ENGINE=1
-export LD_PRELOAD=/root/miniforge3/envs/sglamx-dong1/lib/libiomp5.so:/root/miniforge3/envs/sglamx-dong1/lib/libtcmalloc.so:/root/miniforge3/envs/sglamx-dong1/lib/libtbbmalloc.so.2
+condaenv="sglamx-dong1"
 
-SGLANG_CPU_OMP_THREADS_BIND='0-45|48-94' python3 -m sglang.launch_server \
-    --model /home/dchen/models/qwen3-32b --trust-remote-code --device cpu --disable-overlap-schedule \
+export SGLANG_USE_CPU_ENGINE=1
+export LD_PRELOAD=/root/miniforge3/envs/$condaenv/lib/libiomp5.so:/root/miniforge3/envs/$condaenv/lib/libtcmalloc.so:/root/miniforge3/envs/$condaenv/lib/libtbbmalloc.so.2
+
+# model path:
+# /root/.cache/modelscope/hub/models/Qwen/Qwen3-32B 
+# /root/.cache/modelscope/hub/models/Qwen/Qwen3-235B-A22B-Instruct-2507
+# /root/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-R1-0528   
+
+modelpath="/root/.cache/modelscope/hub/models/Qwen/Qwen3-32B"
+
+SGLANG_CPU_OMP_THREADS_BIND='0-39|40-79' python3 -m sglang.launch_server \
+    --model $modelpath --trust-remote-code --device cpu --disable-overlap-schedule \
     --tp 2 --mem-fraction-static 0.8 --max-total-tokens 63356 --port 30010 --disable-radix-cache #--enable-hierarchical-cache #
