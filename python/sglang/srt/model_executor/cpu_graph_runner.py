12514a7fc95b407990cc1adc3e7a6691662e3f0f
达坤 <zhouyukun.zyk@alibaba-inc.com>
2025-11-26 12:15:24 +0800

    update compile config

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -70,7 +70,7 @@ def patch_model(
             yield torch.compile(
                 torch.no_grad()(model.forward),
                 dynamic=False,
-                mode="reduce-overhead"
+                mode="max-autotune"
             )
         else:
             yield model.forward
@@ -85,9 +85,9 @@ def set_torch_compile_config():
 
     torch._inductor.config.fx_graph_cache = True  # Experimental feature to reduce compilation times, will be on by default in future
     torch._inductor.config.freezing = True
-    torch._dynamo.config.accumulated_cache_size_limit = 16384
+    torch._dynamo.config.accumulated_cache_size_limit = 4096
     if hasattr(torch._dynamo.config, "cache_size_limit"):
-        torch._dynamo.config.cache_size_limit = 16384
+        torch._dynamo.config.cache_size_limit = 4096
     monkey_patch_torch_compile()
 
 
