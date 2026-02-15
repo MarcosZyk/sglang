e1e82a3a20872d66410121192a73eb8c6f4b85fe
khfu <fukaihua@ust.hk>
2026-02-16 01:58:32 +0800

    update chatcompletion message

---
+++ dev/scripts/test_agent_simulator/client.py
@@ -0,0 +1,42 @@
+# client_example.py
+import requests
+import json
+
+URL = "http://127.0.0.1:12306/simulate"
+
+payload = {
+    "n": 4,
+    "n_task": 2,
+    "s_list": [10, 12, 10, 14],
+    "m_list": [2, 4, 2, 6],
+    "a_list": [[100, 100], [100, 100, 10, 150], [15, 15], [100, 100, 10, 150, 12, 150]],
+    "b_list": [
+        [0, 0],
+        [100, 100, 10, 0],
+        [0, 0],
+        [
+            100,
+            100,
+            10,
+            150,
+            12,
+            0,
+        ],
+    ],
+    "roles_list": [
+        ["system", "user"],
+        ["system", "user", "assistant", "user"],
+        ["system", "user"],
+        ["system", "user", "assistant", "user", "assistant", "user"],
+    ],
+    "task_list": [0, 0, 1, 0],
+    "wait_time": [0, 0, 1, 0],
+    "tokenizer_name": "Qwen/Qwen2.5-1.5B-Instruct",
+    "openai_model": "Qwen/Qwen2.5-1.5B-Instruct",
+    "temperature": 0.0,
+}
+
+resp = requests.post(URL, json=payload)
+print(resp.status_code)
+print(resp.json())
+# 若运行后返回 session_id, 可去 sessions/<id>.json 查看完整保存记录
