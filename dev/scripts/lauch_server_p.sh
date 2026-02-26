e5ca44dfaaa6948f41f15ac071274a4e712ab71a
khfu <fukaihua@ust.hk>
2026-02-26 21:20:03 +0800

    update

---
+++ dev/scripts/lauch_server_p.sh
@@ -0,0 +1,15 @@
+CUDA_VISIBLE_DEVICES=$1 python3 -m sglang.launch_server \
+    --mem-fraction-static 0.6 \
+    --context-length 131071 \
+    --disaggregation-mode prefill \
+    --disaggregation-transfer-backend nixl \
+    ----disaggregation-bootstrap-port 8999 \
+    --json-model-override-args '{"rope_scaling": {"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}}' \
+    --model-path Qwen/Qwen3-8B \
+    --port $2 \
+    --enable-mixed-chunk \
+    --chunked-prefill-size 8192 \
+    --enable-cache-report \
+    --disable-overlap-schedule \
+    --enable-artesia
+    #--max-total-tokens 4096 \
