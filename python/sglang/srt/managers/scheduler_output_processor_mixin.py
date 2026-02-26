5c0ce28b0c097dea87e38ca979ca79b79bf623e8
khfu <fukaihua@ust.hk>
2026-02-26 18:05:05 +0800

    update

--- python/sglang/srt/managers/scheduler_output_processor_mixin.py
+++ python/sglang/srt/managers/scheduler_output_processor_mixin.py
@@ -733,7 +733,7 @@ class SchedulerOutputProcessorMixin:
             #len_test = [len(item) for item in decode_ids_list]
         
 
-            #logger.info(f"Detokenizer: {prefill_times}, {finished_reasons}, {decode_times_items}, {len_test}")
+            logger.info(f"Detokenizer: {prefill_times}, {finished_reasons}, {decode_times_items}, {len_test}")
             self.send_to_detokenizer.send_pyobj(
                 BatchTokenIDOut(
                     rids,
