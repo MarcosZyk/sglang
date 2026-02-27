4e904c9c6e7b7c7e98dace1a46eae42fcf9e0ce3
khfu <fukaihua@ust.hk>
2026-02-27 11:24:27 +0800

    update

--- dev/scripts/launch_pd_router.sh
+++ dev/scripts/launch_pd_router.sh
@@ -2,6 +2,8 @@ python -m sglang.srt.disaggregation.launch_lb \
     --policy random \
     --prefill http://127.0.0.1:30000 \
     --decode http://127.0.0.1:30001 \
+    --decode http://127.0.0.1:30002 \
+    --decode http://127.0.0.1:30003 \
     --prefill-bootstrap-ports 8999 \
     --host 0.0.0.0 \
     --port 12347
