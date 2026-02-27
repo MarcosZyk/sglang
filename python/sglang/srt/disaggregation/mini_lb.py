c35194555b7c4eee243f95f1eb7b530e7e82b827
khfu <fukaihua@ust.hk>
2026-02-27 15:24:19 +0800

    update

--- python/sglang/srt/disaggregation/mini_lb.py
+++ python/sglang/srt/disaggregation/mini_lb.py
@@ -106,6 +106,7 @@ class MiniLoadBalancer:
                 ret_json["num_local_cache"] = prefill_json["num_local_cache"]
                 ret_json["num_global_cache"] = prefill_json["num_global_cache"]
                 ret_json["prefill_time"] = ret_json["prefill_time"] + prefill_json["prefill_time"]
+                ret_json["meta_info"]["cached_tokens"] = prefill_json["meta_info"]["cached_tokens"]
 
             return ORJSONResponse(
                 content=ret_json,
