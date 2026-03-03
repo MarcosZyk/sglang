0d07442deae6d4e542d4b1293e32a486f5560c83
达坤 <zhouyukun.zyk@alibaba-inc.com>
2026-03-03 09:11:07 +0800

    extend test length

--- dev/scripts/test_server.py
+++ dev/scripts/test_server.py
@@ -25,15 +25,25 @@ def main():
         base_url=openai_api_base,
     )
     # test_prompt = "San Francisco is a"
-    test_prompt = "Please give some more content based on the following content. San Francisco is a  city that has been around for over 100 years. It was founded in 1849 by the Gold Rush, and it's still growing today. The city is home to many different neighborhoods and districts, each with their own unique character and culture. One of the most popular neighborhoods in San Francisco is Chinatown. This neighborhood is known for its vibrant Chinese-American community, as well as its delicious cuisine and cultural events. If you're visiting San Francisco during your trip, don't miss out on exploring this beloved neighborhood!"
-    start_time = time.time()
+    test_prompt = \
+        ("Please give some more content based on the following content. "
+         "San Francisco is a  city that has been around for over 100 years. "
+         "It was founded in 1849 by the Gold Rush, and it's still growing today. "
+         "The city is home to many different neighborhoods and districts, "
+         "each with their own unique character and culture. "
+         "One of the most popular neighborhoods in San Francisco is Chinatown. "
+         "This neighborhood is known for its vibrant Chinese-American community, "
+         "as well as its delicious cuisine and cultural events. "
+         "If you're visiting San Francisco during your trip, "
+         "don't miss out on exploring this beloved neighborhood!")
+    start_time = time.perf_counter()
     completion = client.completions.create(model=args.model,
                                            prompt=test_prompt, temperature=0.0, max_tokens=2048)
-    # while True:
+
     print("========Question=======")
     print(test_prompt)
     print("=========Answer========")
-    print(time.time() - start_time)
+    print(time.perf_counter() - start_time)
     print(completion.choices[0].text)
 
     response = 0
@@ -47,17 +57,17 @@ def main():
     ]
     idx = 0
 
-    while response < 4 * 1024:
+    while response < 16 * 1024:
 
-        start_time = time.time()
+        start_time = time.perf_counter()
         completion = client.completions.create(model="deepseek-ai/DeepSeek-V2-Lite-Chat",
-                                               prompt=dialect, temperature=0.0, max_tokens=8192)
-        print(time.time() - start_time)
+                                               prompt=dialect, temperature=0.0, max_tokens=16384)
+        print(time.perf_counter() - start_time)
         print(completion.usage.total_tokens)
         print(completion.choices[0].text)
 
         response = completion.usage.total_tokens
-        dialect += dialect + '\nHere\'the answer. ' + completion.choices[
+        dialect += dialect + '\nHere\'s the answer. ' + completion.choices[
             0].text + '\nAll above are previous questions and answers. ' + follow_up[idx]
         idx += 1
 
