d11c4c06deeace2b1d6734888fabd61b1b18d800
达坤 <zhouyukun.zyk@alibaba-inc.com>
2026-03-03 14:24:19 +0800

    fix scripts

--- dev/scripts/bench_server.sh
+++ dev/scripts/bench_server.sh
@@ -1,6 +1,7 @@
 python3 -m sglang.bench_one_batch_server \
     --model-path /sgl-workspace/models/DeepSeek-V2-Lite-Chat \
     --base-url http://127.0.0.1:30010 \
+    --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json \
     --batch-size 1 \
     --input-len 4096 \
     --output-len 256
