0aa7f36ccee0d022c8609de4222e9b0f3c4796a4
达坤 <zhouyukun.zyk@alibaba-inc.com>
2026-03-05 09:42:19 +0800

    rollback query dtype

--- dev/scripts/bm_mla.py
+++ dev/scripts/bm_mla.py
@@ -15,7 +15,7 @@ def bm_mla(seq_len: int, head_num: int, head_block_size: int, split_num: int, ) 
     D = 576
     D_V = 512
 
-    dtype = torch.int8
+    dtype = torch.bfloat16
 
     total_tokens = B * seq_len
     sm_scale = (128 + 64)**-0.5
@@ -32,11 +32,11 @@ def bm_mla(seq_len: int, head_num: int, head_block_size: int, split_num: int, ) 
     round = 61
     param_list = []
     for _ in range(round):
-        q = torch.randn(B, H_Q, D, dtype=torch.bfloat16)
-        k_buffer = torch.randint(-127, 127, [total_tokens, H_KV, D], dtype=dtype)
+        q = torch.randn(B, H_Q, D, dtype=dtype)
+        k_buffer = torch.n(total_tokens, H_KV, D, dtype=dtype)
         v_buffer = k_buffer.narrow(2, 0, D_V)
-        o = torch.zeros(B, H_Q, D_V, dtype=torch.bfloat16)
-        key = torch.randn(B, H_KV, D, dtype=torch.bfloat16)
+        o = torch.zeros(B, H_Q, D_V, dtype=dtype)
+        key = torch.randn(B, H_KV, D, dtype=dtype)
         value = key.narrow(2, 0, D_V)
         attn_logits = torch.empty(
             (B, H_Q, split_num, D_V + 1),
@@ -87,7 +87,8 @@ if __name__ == "__main__":
     head_block_size = args.head_block_size
     split_num = args.split_num
 
-    torch.ops.sgl_kernel.init_cpu_threads_env(args.bind_numa)
-    torch.ops.sgl_kernel.enable_timing(torch.empty([]))
-    bm_mla(seq_len, head_num, head_block_size, split_num)
-    torch.ops.sgl_kernel.export_timing(torch.empty([]))
+    with torch.inference_mode():
+        torch.ops.sgl_kernel.init_cpu_threads_env(args.bind_numa)
+        torch.ops.sgl_kernel.enable_timing(torch.empty([]))
+        bm_mla(seq_len, head_num, head_block_size, split_num)
+        torch.ops.sgl_kernel.export_timing(torch.empty([]))
