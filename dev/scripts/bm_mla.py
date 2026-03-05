ba4ea3912e0e38f47086c04462cefe728d586e47
达坤 <zhouyukun.zyk@alibaba-inc.com>
2026-03-05 10:25:18 +0800

    more round

--- dev/scripts/bm_mla.py
+++ dev/scripts/bm_mla.py
@@ -96,6 +96,6 @@ if __name__ == "__main__":
         print(f"Finish Warmup")
 
         torch.ops.sgl_kernel.enable_timing(torch.empty([]))
-        for i in range(256):
+        for i in range(5):
             bm_mla(seq_len, head_num, head_block_size, split_num)
         torch.ops.sgl_kernel.export_timing(torch.empty([]))
