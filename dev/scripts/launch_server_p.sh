d184de91284c099d5d5b5be972741543f6384b74
达坤 <zhouyukun.zyk@alibaba-inc.com>
2026-04-21 10:09:46 +0800

    update model config

--- dev/scripts/launch_server_p.sh
+++ dev/scripts/launch_server_p.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=$1 python3 -m sglang.launch_server \
     --disaggregation-transfer-backend nixl \
     --disaggregation-bootstrap-port 8999 \
     --json-model-override-args '{"rope_scaling": {"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}}' \
-    --model-path Qwen/Qwen3-8B \
+    --model-path /artesia-workspace/models/Qwen3-8B \
     --port $2 \
     --enable-mixed-chunk \
     --chunked-prefill-size 32768 \
